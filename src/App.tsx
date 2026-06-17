01941e02ad02306b7674f466a58f39bbb88ec46d
Reuven Raz <reuvenrazrz@gmail.com>
2026-06-17 23:01:57 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,19 +8,30 @@ import MySongsPage from './pages/MySongsPage';
 
 function App() {
   return (
-    <LanguageProvider>
-      <BrowserRouter>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/aquarium" element={<AquariumPage />} />
-            <Route path="/my-songs" element={<MySongsPage />} />
-            <Route path="/articles" element={<ArticlesPage />} />
-          </Routes>
-        </Layout>
-      </BrowserRouter>
-    </LanguageProvider>
+    <div style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column', position: 'relative' }}>
+      <LanguageProvider>
+        <BrowserRouter>
+          <Layout>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/aquarium" element={<AquariumPage />} />
+              <Route path="/my-songs" element={<MySongsPage />} />
+              <Route path="/articles" element={<ArticlesPage />} />
+            </Routes>
+          </Layout>
+        </BrowserRouter>
+      </LanguageProvider>
+
+      {/* Root Visitor Counter Badge */}
+      <div style={{ display: 'flex', justifyContent: 'center', padding: '20px', width: '100%', background: 'transparent', zIndex: 9999 }}>
+        <img 
+          src="https://seeyoufarm.com" 
+          alt="visitor counter" 
+        />
+      </div>
+    </div>
   );
 }
+
 
 export default App;
